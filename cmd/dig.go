00b7c0bc0eb8cd0084af48b4c51bf93709b456fa
Daniel Pickens <dpickens1989@gmail.com>
2022-10-30 23:41:01 -0600

    add activity cmds

--- cmd/dig.go
+++ cmd/dig.go
@@ -62,23 +62,3 @@ func init() {
 	rootCmd.AddCommand(digCmd)
 
 }
-
-var ActivityLengthCmd = &cobra.Command{
-	Use:   "ActivityLength",
-	Short: "Checks the time a pod has been in use",
-	Long: `Checks ammount of time a pod has been in use. For example:
-	activitylength <pod-name>,
-
-.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		clientset, err := getClient()
-		if err != nil {
-			panic(err.Error())
-		}
-		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods actively in use in this cluster\n", len(pods.Items))
-	},
-}
