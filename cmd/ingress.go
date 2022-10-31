244e0dede5df940934de5541bf17189965387d6c
Daniel Pickens <dpickens1989@gmail.com>
2022-10-30 23:59:20 -0600

    modified ingress.go

---
+++ cmd/ingress.go
@@ -0,0 +1,116 @@
+package cmd
+
+import (
+	"fmt"
+	"metav1"
+	"context"
+	"time"
+	cobra "github.com/spf13/cobra"
+)
+
+var IngressCmd = &cobra.Command{
+	Use:   "ingress",
+	Short: "Ingress of a resource",
+	Long: `Determines Ingress of a resource. For example:
+	ingress pod <pod-name>`
+
+	Run: func(cmd *cobra.Command, args []string) {
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(IngressCmd)
+}
+
+var (
+	kubeconfig string
+	
+)
+func getClient() (*kubernetes.Clientset, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return clientset, nil
+}
+
+func getPods() (*v1.PodList, error) {
+	clientset, err := getClient()
+	if err != nil {
+		return nil, err
+	}
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return pods, nil
+}
+
+func getPodsByNode() (map[string][]v1.Pod, error) {
+	pods, err := getPods()
+	if err != nil {
+		return nil, err
+	}
+	podsByNode := make(map[string][]v1.Pod)
+	for _, pod := range pods.Items {
+		podsByNode[pod.Spec.NodeName] = append(podsByNode[pod.Spec.NodeName], pod)
+	}
+	return podsByNode, nil
+}
+
+func getPodsByNamespace() (map[string][]v1.Pod, error) {
+	pods, err := getPods()
+	if err != nil {
+		return nil, err
+	}
+	podsByNamespace := make(map[string][]v1.Pod)
+	for _, pod := range pods.Items {
+		podsByNamespace[pod.Namespace] = append(podsByNamespace[pod.Namespace], pod)
+	}
+	return podsByNamespace, nil
+}
+
+func getPodsByDeployment() (map[string][]v1.Pod, error) {
+	pods, err := getPods()
+	if err != nil {
+		return nil, err
+	}
+	podsByDeployment := make(map[string][]v1.Pod)
+	for _, pod := range pods.Items {
+		podsByDeployment[pod.Labels["app"]] = append(podsByDeployment[pod.Labels["app"]], pod)
+	}
+	return podsByDeployment, nil
+}
+
+func getPodsByService() (map[string][]v1.Pod, error) {
+	pods, err := getPods()
+	if err != nil {
+		return nil, err
+	}
+	podsByService := make(map[string][]v1.Pod)
+	for _, pod := range pods.Items {
+		podsByService[pod.Labels["app"]] = append(podsByService[pod.Labels["app"]], pod)
+	}
+	return podsByService, nil
+}
+
+func getPodsByIngress() (map[string][]v1.Pod, error) {
+	pods, err := getPods()
+	if err != nil {
+		return nil, err
+	}
+	podsByIngress := make(map[string][]v1.Pod)
+	for _, pod := range pods.Items {
+		podsByIngress[pod.Labels["app"]] = append(podsByIngress[pod.Labels["app"]], pod)
+	}
+	return podsByIngress, nil
+}
